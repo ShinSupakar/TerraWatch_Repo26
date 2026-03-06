39850f5bd7c5d26c0249906d656eee584ee13aa4
ShinSupakar <shinjinisupakar@gmail.com>
2026-03-06 22:04:22 +0800

    feat: sync catalog and build assets

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.qfiio9p1eb8"
+    "revision": "0.d2kbg328ma4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
